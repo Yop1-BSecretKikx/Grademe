0afa7b9c0698e182d01a7842eb12c771dba2b156
Yop1 <drakeyop95@gmail.com>
2025-10-02 17:14:28 +0100

    Delete Fetch_test directory

--- Fetch_test/run.sh
+++
@@ -1,8 +0,0 @@
-gcc -o grademe-macos grademe.c \
-    -I/opt/homebrew/Cellar/mysql-client/9.4.0/include \
-    -L/opt/homebrew/Cellar/mysql-client/9.4.0/lib \
-    -lmysqlclient \
-    $(pkg-config --cflags --libs glib-2.0) \
-    -lsqlite3
-
-./grademe-macos
